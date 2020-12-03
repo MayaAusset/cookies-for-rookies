b6cc1c50a7e9cfee27e649ad4134da539420db13
Maya Ausset <mayaausset@gmail.com>
2020-12-03 12:29:26 +0100

    added routes for recipes, tests on postman work

--- server/routes/recipes.routes.js
+++ server/routes/recipes.routes.js
@@ -1,10 +1,13 @@
 const { Router } = require("express");
+const mongoose = require("mongoose");
 const router = Router();
 
 const Recipe = require("../models/recipemodel");
 //const User = require("../models/usermodel");
 
-router.post("/projects", (req, res) => {
+
+//! CREATE A RECIPE
+router.post("/recipe", (req, res) => {
     const { image, title, duration, ingredients, description, fromUser } = req.body;
     console.log("body", req.body);
   
@@ -23,5 +26,43 @@ router.post("/projects", (req, res) => {
         res.status(500).json(err);
       });
   });
+
+
+//! DISPLAY ALL RECIPES
+router.get("/recipes", (req, res) => {
+    Recipe.find()
+      .populate("recipes")
+      .then((allTheRecipes) => {
+        res.status(200).json(allTheRecipes);
+      })
+      .catch((err) => {
+        res.status(500).json(err);
+      });
+  });
+
+
+  //! GET ONE SPECIFIC RECIPE TO DISPLAY
+
+  router.get("/recipes/:id", (req, res) => {
+    const { id } = req.params;
+  
+    // Check if the incoming id is a valid ObjectId type
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      res.status(400).json({ message: "Specified id is not valid" });
+      return;
+    }
+  
+    // Our projects have array of tasks' ids and
+    // we can use .populate() method to get the whole task objects
+    Recipe.findById(id)
+      .populate("recipes")
+      .then((recipe) => {
+        res.status(200).json(recipe);
+      })
+      .catch((error) => {
+        res.status(500).json(error);
+      });
+  });
+  
 
   module.exports = router;
